841dae74b18bd8946426a97ecc09260b361ca6d9
Fritz Paz <fritz@pazmail.ch>
2022-02-28 13:54:03 -0500

    Pushing Project Setup

--- Herder/Assets/Scripts/SceneManager.cs
+++ Herder/Assets/Scripts/SceneManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rScriptviourScript : MonoBehaviour
+public class SceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       s
+        
     }
 
     // Update is called once per frame
