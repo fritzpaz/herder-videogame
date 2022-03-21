088fa8d67c1893a9d90069b172049da275bcd478
Ben <bmann@rollins.edu>
2022-03-21 19:51:07 -0400

    Fixes

---
+++ Herder/Assets/GenerationTesting/CarTag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTag : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log("collided with " + collision.gameObject.name);
+    }
+
+}
