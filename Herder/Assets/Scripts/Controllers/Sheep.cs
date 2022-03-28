9cdb038b54efea039e7ece5f152fa4b6f33ecdd7
Fritz Paz <fritz@pazmail.ch>
2022-03-28 17:29:58 -0400

    Small Lake Changes (Ben's Work)
    Tried changing collider box to see if gameplay would be better
    Moved lake script into sheep for simplicity

--- Herder/Assets/Scripts/Controllers/Sheep.cs
+++ Herder/Assets/Scripts/Controllers/Sheep.cs
@@ -62,21 +62,27 @@ public class Sheep : MonoBehaviour
         }
     }
 
+    // When sheep is destroyed, reduce sheepCount
     private void OnDestroy()
     {
         sm.sheepCount -= 1;
-        
     }
-    private void OnCollisionEnter2D(Collision2D collison){ //If Sheep Collides with Lake Fade
-         if (collison.gameObject.tag == "Lake"){
-            StartCoroutine("fadeSprite");
-         }
+
+    // Handle Sheep's collision
+    private void OnCollisionEnter2D(Collision2D collison){
+        //If Sheep Collides with Lake, Make the Sheep Fade then Destroy itself
+        if (collison.gameObject.tag == "Lake"){
+            StartCoroutine("FadeSheepSpriteInLake");        // Call IEnumerator 
+            Destroy(gameObject);                            // Destroy after Coroutine is completed
+        }
     }
        
-   IEnumerator fadeSprite(){
-       for (float f =1f; f >= -0.05f; f-= 0.05f){
+    // Allows for timing the fading/destruction of sheep.
+   IEnumerator FadeSheepSpriteInLake(){
+       // Fade sheep a bit every 0.05 seconds
+       for (float f =1f; f > 0f; f-= 0.05f){
            Color c = fade.material.color;
-           c.a = f;
+           c.a = f;                                         // Changing alpha (transparency)
            fade.material.color = c;
            yield return new WaitForSeconds(0.05f);
        }
