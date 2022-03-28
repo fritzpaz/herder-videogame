c58db13500a954f656d1c7c514d40dafe4153c6b
Ben <bmann@rollins.edu>
2022-03-28 17:20:27 -0400

    Organization and Timing fixes for destroy

--- Herder/Assets/Scripts/Terrain/LakeBehavior.cs
+++ Herder/Assets/Scripts/Terrain/LakeBehavior.cs
@@ -7,7 +7,7 @@ public class LakeBehavior : MonoBehaviour
   
     private void OnCollisionEnter2D(Collision2D collison){
          if (collison.gameObject.tag == "Sheep"){
-            Destroy(collison.gameObject,2);
+            Destroy(collison.gameObject,1);
          }
         
     }
