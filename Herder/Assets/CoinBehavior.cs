140ebfda13943992b53c422c5c76cf2703e03559
Ben Mann <bmann@rollins.edu>
2022-04-04 14:13:26 -0400

    Added Coin Prefab

---
+++ Herder/Assets/CoinBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OntriggerEnter(Collider2D collision){
+        if(collision.gameObject.tag == "Sheep"){
+            Destroy(collision.gameObject);
+        }
+    }
+}
